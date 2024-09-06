b9eba1f6e6bf73ed70b9fbd00c8510ad7321d3e9
Thibault Garcia <thibault.garcia.pro@gmail.com>
2024-09-06 16:20:34 +0200

    exo5 - add test1

--- src/test/java/tibo/tdd/exo5/ShopTest.java
+++ src/test/java/tibo/tdd/exo5/ShopTest.java
@@ -3,6 +3,7 @@ package tibo.tdd.exo5;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 public class ShopTest extends Assertions {
     private static Shop shop;
@@ -19,9 +20,23 @@ public class ShopTest extends Assertions {
         product.setType("Produit laitier");
         product.setName("Raclette");
         product.setSellIn(10);
-        product.setQuality(50);
+        product.setQuality(30);
 
         shop.clearProducts();
         shop.addProduct(product);
+    }
+
+    @Test
+    public void testShop_WhenUpdate_IfProductQuality_50_AndProductSellIn_0_ThenProductQuality_25() {
+        int quality = 50;
+        product.setQuality(quality);
+        int sellIn = 0;
+        product.setSellIn(sellIn);
+        int expected = 25;
+
+        shop.update(product);
+        int actual = product.getQuality();
+
+        assertEquals(expected, actual);
     }
 }

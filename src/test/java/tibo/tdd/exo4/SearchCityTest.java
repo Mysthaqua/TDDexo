84ecec6786dbd6d3615e02b0d64b334513f1dc5e
Thibault Garcia <thibault.garcia.pro@gmail.com>
2024-09-06 12:23:00 +0200

    exo4 - add test1

---
+++ src/test/java/tibo/tdd/exo4/SearchCityTest.java
@@ -0,0 +1,30 @@
+package tibo.tdd.exo4;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
+import tibo.tdd.exo4.exception.NotFoundException;
+
+import java.util.List;
+
+public class SearchCityTest extends Assertions {
+    private static SearchCity searchCity;
+
+    @BeforeAll
+    public static void init() {
+        searchCity = new SearchCity(List.of("Paris", "Budapest", "Skopje", "Rotterdam", "Valence", "Vancouver", "Amsterdam", "Vienne", "Sydney", "New York", "Londres", "Bangkok", "Hong Kong", "Dubaï", "Rome", "Istanbul"));
+    }
+
+    @Test
+    @DisplayName("Test de la méthode search de la classe SearchCity, avec city = \"P\", lance une NotFoundException")
+    public void testSearchCity_WhenSearch_IfCity_P_ThrowsNotFoundException() {
+        String city = "P";
+        Class<NotFoundException> expectedType = NotFoundException.class;
+
+        Executable executable = () -> searchCity.search(city);
+
+        assertThrows(expectedType, executable);
+    }
+}
